f8eeef5c69fe86c6aa70314119659082ad24a7be
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-08-01 19:55:22 +0100

    a

--- Readme.md
+++ Readme.md
@@ -1,2 +1,5 @@
 # app
  
+# step 1
+connecter vous à mongodb avec un projet nommé bookstore (nom de la BD)
+créer une connexion à mongodb et une entié book avec : titre (unique), auteur: requis;prix >=1 , edition :chaine
--- mongoosedemo/index.js
+++ mongoosedemo/index.js
@@ -27,3 +27,38 @@ const UserSchema=new mongoose.Schema(
 );
 
 const User=mongoose.model("User",UserSchema);
+/*const createUser = async () => {
+    const newUser = new User({
+    name: 'John Doe',
+    age: 30,
+    email: 'john.doe@example.com'
+    });
+    try {
+    const savedUser = await newUser.save();
+    console.log('User saved:', savedUser);
+    } catch (error) {
+    console.error('Error saving user:', error);
+    }
+    };
+    createUser();
+    */
+    const getUsers = async () => {
+        try {
+        const users = await User.find({name:"John Doe",age:35});
+        console.log('Users:', users);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        }
+        };
+        getUsers();
+        const updateUser = async (id) => {
+            try {
+            const updatedUser = await User.findByIdAndUpdate(id, { age: 45 }, { new: true });
+            console.log('Updated user:', updatedUser);
+            } catch (error) {
+            console.error('Error updating user:', error);
+            }
+            };
+            updateUser('66ab9976c0765f418ea67a72');
+    getUsers();
+           
